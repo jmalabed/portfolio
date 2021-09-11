633414cc88db9c973cd6651687dec55213c1dc3d
jmalabed <jmalabed27@gmail.com>
2021-09-10 21:31:54 -0700

    updating website

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express")
 const app = express()
 const PORT = 3000;
 
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+
 // index route
 app.get("/",(req,res)=>{
   res.render('index.ejs')
@@ -14,7 +18,8 @@ app.get("/",(req,res)=>{
 
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 
 // Listen Route
 app.listen(PORT, ()=>{
