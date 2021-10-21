9e08fe2b863222aba8c918a3d1f55d47da62e2db
jmalabed <jmalabed27@gmail.com>
2021-10-20 23:01:25 -0700

    update styling, color theme, and add project page.

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 // DEPENDENCIES
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const PORT = 3000;
 
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 // index route
-app.get("/",(req,res)=>{
-  res.render('index.ejs')
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs");
+});
 
-
-
-
-
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride("_method"))
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // Listen Route
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log("listening on PORT", PORT);
-})
+});
