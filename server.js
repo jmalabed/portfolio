b5e6980f071d7f6a7b4ae33c3635cb673ae27fe7
jmalabed <jmalabed27@gmail.com>
2021-10-21 10:44:32 -0700

    added styling, multiple pages, and some images

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get("/projects", (req, res) => {
   res.render("projects.ejs");
 });
 
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
